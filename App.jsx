e7d0a0ceecc757535cfd229441c0a1caf7112d88
tara1181 <84344662+tara1181@users.noreply.github.com>
2021-06-06 00:00:43 -0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Footer />
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>java script and rect.js</h1>
+      <p>
+        this was amazing bootcamp taken up by Tara Chand Kumawat we convered
+        everything from scratch including javascript React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
